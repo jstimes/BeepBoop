6ed4f1eec9cfbc88e11b6f1811898fc4327ef55e
Jacob Stimes <jacob.stimes@gmail.com>
2021-10-24 12:53:55 -0700

    MVP w/ sound

---
+++ main.js
@@ -0,0 +1,188 @@
+const RED = 1;
+const GREEN = 2;
+const BLUE = 3;
+const YELLOW = 4;
+
+let gameState;
+let redButton;
+let greenButton;
+let yellowButton;
+let blueButton;
+let allButtons;
+let nextRoundButton;
+
+
+class Sound {
+  constructor(assetName) {
+    this.audioElement_ = document.createElement('audio');
+    this.audioElement_.src = `./assets/audio/${assetName}`;
+    this.audioElement_.addEventListener('ended', () => {
+      this.isPLaying_ = false;
+    });
+    this.isPlaying_ = false;
+  }
+
+  play() {
+    if (this.isPlaying_) {
+      this.audioElement_.pause();
+      this.audioElement_.load();
+    }
+    this.audioElement_.play();
+    this.isPlaying_ = true;
+  }
+}
+
+const SOUNDS_FAMILY = 'dream_dancer_';
+const SOUNDS_EXT = '.wav';
+let redSound = new Sound(`${SOUNDS_FAMILY}C${SOUNDS_EXT}`);
+let greenSound = new Sound(`${SOUNDS_FAMILY}Dm${SOUNDS_EXT}`);
+let yellowSound = new Sound(`${SOUNDS_FAMILY}Em${SOUNDS_EXT}`);
+let blueSound = new Sound(`${SOUNDS_FAMILY}F${SOUNDS_EXT}`);
+
+
+function onLoad() {
+  if (document.readyState !== 'complete') {
+    return;
+  }
+  gameState = new GameState();
+  redButton = document.getElementById('red-button');
+  greenButton = document.getElementById('green-button');
+  yellowButton = document.getElementById('yellow-button');
+  blueButton = document.getElementById('blue-button');
+  allButtons = [redButton, greenButton, blueButton, yellowButton];
+
+  nextRoundButton = document.getElementById('next-round-button');
+  hideNextRoundButton();
+  nextRoundButton.onclick = () => { gameState.startNextRound(); };
+
+  document.getElementById('start-button').onclick = () => {
+    gameState.start();
+    document.getElementById('start-controls').classList.add('hidden');
+  }
+}
+
+class GameState {
+
+  constructor() {
+    this.goalRounds = 0;
+    this.isStarted = false;
+    this.sequence = [];
+    this.inputSequence = [];
+  }
+
+  start() {
+    this.isStarted = true;
+    this.startNextRound();
+  }
+
+  startNextRound() {
+    hideNextRoundButton();
+    this.sequence.push(getNextButton());
+    this.flashSequence();
+  }
+
+  async flashSequence() {
+    for (let i = 0; i < this.sequence.length; i++) {
+      await this.pressButton(this.sequence[i]);
+    }
+    this.enableInput();
+  }
+
+  enableInput() {
+    this.inputSequence = [];
+    allButtons.forEach((btn) => {
+      const value = parseInt(btn.getAttribute('value'));
+      btn.onclick = undefined;
+      btn.onclick = () => {
+        this.userPressedButton(value);
+      };
+    })
+  }
+
+  async userPressedButton(buttonNum) {
+    await this.pressButton(buttonNum);
+    this.inputSequence.push(buttonNum);
+    for (let i = 0; i < this.inputSequence.length; i++) {
+      if (this.inputSequence[i] != this.sequence[i]) {
+        alert('lost');
+        return;
+      }
+    }
+    if (this.inputSequence.length === this.goalRounds) {
+      alert('you win yay');
+      return;
+    }
+    if (this.inputSequence.length === this.sequence.length) {
+      showNextRoundButton();
+      return;
+    }
+  }
+
+  pressButton(buttonNum) {
+    return new Promise((resolve, reject) => {
+      this.playSound(buttonNum);
+      const buttonEl = getButtonElement(buttonNum);
+      buttonEl.classList.add('pressed');
+      setTimeout(() => {
+        buttonEl.classList.remove('pressed');
+        setTimeout(() => {
+          resolve();
+        }, 250);
+      }, 250);
+    });
+  }
+
+  playSound(buttonNum) {
+    if (buttonNum === RED) {
+      redSound.play();
+    }
+    if (buttonNum === GREEN) {
+      greenSound.play();
+    }
+    if (buttonNum === BLUE) {
+      blueSound.play();
+    }
+    if (buttonNum === YELLOW) {
+      yellowSound.play();
+    }
+  }
+
+}
+
+function getButtonElement(buttonNum) {
+  if (buttonNum === RED) {
+    return redButton;
+  }
+  if (buttonNum === GREEN) {
+    return greenButton;
+  }
+  if (buttonNum === BLUE) {
+    return blueButton;
+  }
+  if (buttonNum === YELLOW) {
+    return yellowButton;
+  }
+}
+
+function getNextButton() {
+  const rand = Math.random();
+  if (rand < 0.25) {
+    return RED;
+  } else if (rand < 0.5) {
+    return GREEN;
+  } else if (rand < 0.75) {
+    return BLUE;
+  } else {
+    return YELLOW;
+  }
+}
+
+function hideNextRoundButton() {
+  nextRoundButton.classList.add('hidden');
+}
+
+function showNextRoundButton() {
+  nextRoundButton.classList.remove('hidden');
+}
+
+document.onreadystatechange = onLoad;
